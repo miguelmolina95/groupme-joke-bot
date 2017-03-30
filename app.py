699aa80a287e18eca964b8156d928f3456703918
miguelmolina95 <mamolina@umich.edu>
2017-03-29 20:42:35 -0400

    return

--- app.py
+++ app.py
@@ -24,5 +24,7 @@ def chat():
 		if 'hello joke bot' in message['text'].lower():
 			send_message('Greetings human if you would like to find out more about me visit at my home page:  https://joke-bot-486.herokuapp.com/')
 
+	return "ok", 200
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000, debug=True)
